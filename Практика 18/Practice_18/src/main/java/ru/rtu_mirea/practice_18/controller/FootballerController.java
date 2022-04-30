b9937a73c5f8d41b5ba5ed123cd34d938267e5e9
Mikhail OKhapkin <mikhail.okhapkin@yandex.ru>
2022-04-30 21:12:20 +0300

    Added drafts for Practices 17 and 18

---
+++ Практика 18/Practice_18/src/main/java/ru/rtu_mirea/practice_18/controller/FootballerController.java
@@ -0,0 +1,47 @@
+package ru.rtu_mirea.practice_18.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.rtu_mirea.practice_18.model.Footballer;
+import ru.rtu_mirea.practice_18.model.Team;
+import ru.rtu_mirea.practice_18.service.FootballerService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class FootballerController {
+    @Autowired
+    private FootballerService service;
+
+    @PostMapping("/footballer")
+    public void add(@RequestBody Footballer footballer) {
+        service.addFootballer(footballer);
+    }
+
+    @GetMapping("/footballers")
+    public List<Footballer> getAll() {
+        return service.getFootballers();
+    }
+
+    @GetMapping("/footballer/{id}")
+    public List<Footballer> get(@PathVariable UUID id) {
+        return service.getFootballer(id);
+    }
+
+    @DeleteMapping("/footballer/{id}")
+    public void delete(@PathVariable UUID id) {
+        service.deleteFootballer(id);
+    }
+
+    @GetMapping(value = "/footballer/{footballerId}/team")
+    public @ResponseBody Team getFootballerTeam(@PathVariable("footballerId") Long footballerId) {
+        return service.getFootballerByTeam(footballerId);
+    }
+
+    @GetMapping("/footballer/sort_f/{c}")
+    public List sort_t(@PathVariable("c") String Criteria) {
+        return service.SortCriteria(Criteria);
+    }
+}
---
+++ Практика 18/Practice_18/src/main/java/ru/rtu_mirea/practice_18/service/TeamService.java
@@ -0,0 +1,79 @@
+package ru.rtu_mirea.practice_18.service;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import ru.rtu_mirea.practice_18.model.Footballer;
+import ru.rtu_mirea.practice_18.model.Team;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+@Service
+public class TeamService {
+    private final SessionFactory sessionFactory;
+    private Session session;
+
+    @PostConstruct
+    public void init() {
+        session = sessionFactory.openSession();
+    }
+
+    @PreDestroy
+    public void unSession() {
+        session.close();
+    }
+
+    public void addTeam(Team team) {
+        session.beginTransaction();
+        session.saveOrUpdate(team);
+        session.getTransaction().commit();
+    }
+
+    public List<Team> getTeams() {
+        return session.createQuery("select t from Team t", Team.class).list();
+    }
+
+    public List<Team> getTeam(UUID id) {
+        return session.createQuery("select t from Team t where t.id ='" + id + "'", Team.class).list();
+    }
+
+    public void deleteTeam(Team team) {
+        session.beginTransaction();
+
+        List<Team> query = session.createQuery("select t from Team t where t.name = '" + team.getName() + "'" + " and t.creationDate = '" + team.getCreationDate() + "'", Team.class).list();
+        for (Team t : query) {
+            session.delete(t);
+        }
+
+        session.getTransaction().commit();
+    }
+
+    public void deleteTeam(UUID id) {
+        session.beginTransaction();
+
+        Team temp = session.load(Team.class, id);
+        session.delete(temp);
+
+        session.getTransaction().commit();
+    }
+
+    public List SortCriteria (String Criteria) {
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Footballer> footballerCriteriaQuery = builder.createQuery(Footballer.class);
+        Root<Footballer> root = footballerCriteriaQuery.from(Footballer.class);
+        footballerCriteriaQuery.select(root).orderBy(builder.asc(root.get(Criteria)));
+        Query query = session.createQuery(footballerCriteriaQuery);
+        return query.getResultList();
+    }
+}
